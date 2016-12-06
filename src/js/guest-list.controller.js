1c1ab56c4facc47e8b95e8dee44d36fe59f9f25a
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-06 15:03:43 -0500

    created a controller for the guestList, injected with the service.

---
+++ src/js/guest-list.controller.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular.module('hotelier')
+    .controller('GuestListController', GuestListController);
+
+  GuestListController.$inject = ['GuestListService'];
+
+  function GuestListController(GuestListService) {
+
+  };
+
+
+}());
