c01eb41190fe388f319609f6a158d68da2baf91b
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-06 16:41:00 -0500

    created a function on the controllers scope that calls the  request once the form is submitted.

--- src/js/guest-list.controller.js
+++ src/js/guest-list.controller.js
@@ -1,13 +1,19 @@
 (function() {
   'use strict';
-
   angular.module('hotelier')
     .controller('GuestListController', GuestListController);
 
   GuestListController.$inject = ['GuestListService'];
 
   function GuestListController(GuestListService) {
+     this.inputValues = {};
 
+     this.createGuest = function createGuest() {
+       console.log('it worked');
+
+       GuestListService.postGuest(this.inputValues.fullname, this.inputValues.email, this.inputValues.phone);
+
+     }
   };
 
 
