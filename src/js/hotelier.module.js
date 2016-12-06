99e5e5700ed26d8fe9efde8322b7df265f9166f9
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-06 16:59:53 -0500

    Added ui.router dependency and injected the  into the routerConfig function. to connect templates to index.html

--- src/js/hotelier.module.js
+++ src/js/hotelier.module.js
@@ -1,6 +1,22 @@
 (function() {
   'use strict';
 
-  angular.module('hotelier', []);
+  angular.module('hotelier', ['ui.router'])
+    .config(routerConfig);
+
+  routerConfig.$inject = ['$stateProvider'];
+
+  function routerConfig() {
+
+    $stateProvider
+      .state({
+        name: 'AddGuest',
+        url: '/create-guest',
+        templateUrl: '../views/guest-form.template.html',
+        controller: 'GuestListController',
+        controllerAs: 'guestlist'
+      });
+
+  }
 
 }());
