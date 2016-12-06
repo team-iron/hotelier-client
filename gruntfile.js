bf1bfc37bdf944785d3dd81a115081a4b614918b
Navi Kaur <nkaur901@gmail.com>
2016-12-06 14:01:47 -0500

    set up build and grunt to watch for changes

--- gruntfile.js
+++ gruntfile.js
@@ -4,17 +4,61 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
+        clean: ['build/'],
+
+        jshint: {
+
+            options: {
+                  jshintrc: '.jshintrc',
+                  ignores: ['node_modules/**']
+            },
+            source: {
+                  files: {
+                      src: ['src/js/**/*.js']
+                  }
+            }
+        },
+
         copy: {
             html: {
                 files: [
                     { expand: true, cwd: 'src/', src: 'index.html', dest: 'build/' }
                 ]
+            },
+            vendorjs: {
+                files: [{
+                    expand: true,
+                    cwd: 'node_modules/angular/',
+                    src: ['angular.js'],
+                    dest: 'build/js/'
+                }]
+            }
+        },
+        concat: {
+              js: {
+                src: ['build/js/angular.js', 'src/js/hotelier.module.js', 'src/js/**/*.js'],
+                dest: 'build/js/app.js'
+              }
+        },
+        watch: {
+            html: {
+                files: ['src/index.html'],
+                tasks: ['copy:html']
+            },
+            js: {
+                files: ['src/js/**/*.js'],
+                tasks: ['test', 'concat']
             }
         }
 
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', [ 'copy' ]);
+    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('default', [ 'clean', 'test', 'copy', 'concat' ]);
 };
