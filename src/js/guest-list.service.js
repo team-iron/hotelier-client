7e9c2415f5d90fa09bbabf358904f2250f4b893b
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-07 09:40:50 -0500

    Added DocBlockr on  function to let future developers know the paramaters required for the call to be successfull.

--- src/js/guest-list.service.js
+++ src/js/guest-list.service.js
@@ -12,6 +12,14 @@
       postGuest: postGuest
     };
 
+
+    /**
+     * Ajax call to create a new guest
+     * @param  {String} fullname Full name rewquired to fill out form
+     * @param  {String} email    Email required to fill out the form
+     * @param  {String} phone    Phone number which will be a string
+     * @return {Promise}         Promise that the ajax call will return when complete
+     */
     function postGuest(fullname, email, phone) {
       console.log('here');
       return $http({
