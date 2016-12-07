a37422de423387da2805165a558bfab91a2416ab
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-07 10:24:00 -0500

    Updated DocBlockr to include an id parameter into the  request.

--- src/js/guest-list.service.js
+++ src/js/guest-list.service.js
@@ -8,7 +8,7 @@
 
   /**
    * Service for Guest List Constructor Fn
-   * @param {Service} $http Ajax call injection
+   * @param {Service}  Ajax call injection
    * @return {Object} of functions within the main Constructor
    */
   function GuestListService($http) {
@@ -20,31 +20,35 @@
 
     /**
      * Ajax call to create a new guest
-     * @param  {String} fullname Full name rewquired to fill out form
+     * @param  {String} fullName Full name rewquired to fill out form
      * @param  {String} email    Email required to fill out the form
      * @param  {String} phone    Phone number which will be a string
+     * @param  {string} id        Id given to a staff member once logged in in order to create a guest
      * @return {Promise}         Promise that the ajax call will return when complete
      */
-    function postGuest(fullname, email, phone) {
+    function postGuest(fullName, email, phone, id) {
       console.log('here');
       return $http({
         url: 'https://hotelier-api-iron.herokuapp.com/api/Guests',
         data: {
-          fullname: fullname,
+          fullName: fullName,
           email: email,
           phone: phone
         },
-        method: 'post'
+        method: 'post',
+        headers: {
+          'Authorization': id
+        }
       })
-      .then(function ReturnData(response) {
-        console.log('passed');
+      .then(function ReturnData(data) {
+        console.log('passed', data);
 
       });
 
 
-    };
+    }
 
-  };
+  }
 
 
 }());
