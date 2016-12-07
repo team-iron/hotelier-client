d513b5cc73846fd6fd0f550583b3c8a198ffbbc1
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-07 10:22:20 -0500

    Added DocBlockr and updated api endpoints in my js files from fullname to fullName.

--- src/js/guest-list.controller.js
+++ src/js/guest-list.controller.js
@@ -5,16 +5,21 @@
 
   GuestListController.$inject = ['GuestListService'];
 
+  /**
+   * Guest List constructor function calling the functions from the service into its
+   * local scope to pass onto the html.
+   * @param {Service} GuestListService Controllers injected service
+   */
   function GuestListController(GuestListService) {
      this.inputValues = {};
 
      this.createGuest = function createGuest() {
        console.log('it worked');
 
-       GuestListService.postGuest(this.inputValues.fullname, this.inputValues.email, this.inputValues.phone);
+       GuestListService.postGuest(this.inputValues.fullName, this.inputValues.email, this.inputValues.phone, this.inputValues.id);
 
-     }
-  };
+     };
+  }
 
 
 }());
