fba9b607d8b342b95d09294d34eb25c89b039c8b
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-08 13:36:39 -0500

    Started building the services constructor function.

---
+++ src/js/create-reservation.service.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular.module('hotelier')
+    .factory('CreateReservationService', CreateReservationService);
+
+  function CreateReservationService() {
+    return {
+
+    };
+  }
+}());
