aef78a4f3998ac597a4bd5237dcbea79245d5bf0
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-08 15:34:00 -0500

    Added the injected service into the argument of the controllers Constructor function.

--- src/js/create-reservation.controller.js
+++ src/js/create-reservation.controller.js
@@ -1,12 +1,22 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('hotelier')
-    .controller('CreateReservationController', CreateReservationController);
+    angular.module('hotelier')
+        .controller('CreateReservationController', CreateReservationController);
 
-  CreateReservationController.$inject = ['CreateReservationService']
+    CreateReservationController.$inject = ['CreateReservationService'];
 
-  function CreateReservationController(CreateReservationService){
+    function CreateReservationController(CreateReservationService) {
+        this.reservationDetails = {};
 
-  }
+        this.addReservation = function addReservation() {
+            CreateReservationService.createReservation(
+                this.ReservationDetails.checkinDate,
+                this.ReservationDetails.checkoutDate,
+                this.ReservationDetails.numberOfGuests,
+                this.ReservationDetails.guestId,
+                this.ReservationDetails.roomId
+            );
+        };
+    }
 }());
