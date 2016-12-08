7c039dd6f5a93f464f6660a41e0ccaae9db95c49
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-08 13:35:11 -0500

    Created a new controller for the creation of the reservation.

---
+++ src/js/create-reservation.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular.module('hotelier')
+    .controller('CreateReservationController', CreateReservationController);
+
+  CreateReservationController.$inject = ['CreateReservationService']
+
+  function CreateReservationController(CreateReservationService){
+
+  }
+}());
