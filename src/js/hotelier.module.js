b554031ce50ef4ddab1d88ee38d9eb41bf4cf3a9
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-08 15:42:54 -0500

    Proper indentation of .

--- src/js/hotelier.module.js
+++ src/js/hotelier.module.js
@@ -1,46 +1,46 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('hotelier', ['ui.router'])
-  .config(viewConfig);
+    angular.module('hotelier', ['ui.router'])
+        .config(viewConfig);
 
-  viewConfig.$inject = ['$stateProvider'];
+    viewConfig.$inject = ['$stateProvider'];
 
-  function viewConfig($stateProvider){
-    $stateProvider
-    .state({
-      name: 'home',
-      url: '',
-      templateUrl:'views/home.template.html'
-    })
-    .state({
-      name: 'staff-login',
-      url: '/staff-login',
-      templateUrl: 'views/staff-login.template.html',
-      controller: 'LoginController',
-      controllerAs: 'login'
-    })
-    .state({
-      name: 'add-guest',
-      url: '/add-guest',
-      templateUrl: 'views/guest-form.template.html',
-      controller: 'GuestListController',
-      controllerAs: 'guestlist'
-    })
-    .state({
-      name: 'reservations',
-      url: '/reservations',
-      templateUrl: 'views/reservations.template.html',
-      controller: ''
-    })
-    .state({
-      name: 'create-reservation',
-      url: '/create-reservation',
-      templateUrl: 'views/create-reservation.template.html',
-      controller: 'CreateReservationController',
-      controllerAs: 'create-reservation'
-    });
-  }
+    function viewConfig($stateProvider) {
+        $stateProvider
+            .state({
+                name: 'home',
+                url: '',
+                templateUrl: 'views/home.template.html'
+            })
+            .state({
+                name: 'staff-login',
+                url: '/staff-login',
+                templateUrl: 'views/staff-login.template.html',
+                controller: 'LoginController',
+                controllerAs: 'login'
+            })
+            .state({
+                name: 'add-guest',
+                url: '/add-guest',
+                templateUrl: 'views/guest-form.template.html',
+                controller: 'GuestListController',
+                controllerAs: 'guestlist'
+            })
+            .state({
+                name: 'reservations',
+                url: '/reservations',
+                templateUrl: 'views/reservations.template.html',
+                controller: ''
+            })
+            .state({
+                name: 'create-reservation',
+                url: '/create-reservation',
+                templateUrl: 'views/create-reservation.template.html',
+                controller: 'CreateReservationController',
+                controllerAs: 'createReservation'
+            });
+    }
 
 
 }());
