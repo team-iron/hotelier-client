cbfed0b6a70dc4611fd62e14c538e0ec5673b317
ngalantowicz <ngalantowicz@gmail.com>
2016-12-08 13:14:34 -0500

    saved work to start new branch

--- src/js/reservations.controller.js
+++ src/js/reservations.controller.js
@@ -9,14 +9,18 @@
     function ReservationsController( LoginService, ReservationsService ) {
 
         var vm = this;
-        this.userToken = LoginService.loginToken;
-        this.reservations = null;
+        this.userToken = LoginService.loginToken();
+        this.reservations = JSON.parse(sessionStorage.getItem('reservations')) || null;
 
         this.getReservations = function getReservations(token) {
             ReservationsService.getReservations(token)
                 .then(function success(data) {
                     vm.reservations = data;
+                    sessionStorage.setItem('reservations', JSON.stringify(vm.reservations));
                     console.log(vm.reservations);
+                })
+                .catch(function error(xhr) {
+                    console.log(xhr);
                 });
         };
     }
