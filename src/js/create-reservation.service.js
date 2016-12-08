2abb491628c6a87adae22aa35deda9f7026837ab
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-08 15:33:11 -0500

    Added the  injection into the argument of the service Constructor function.

--- src/js/create-reservation.service.js
+++ src/js/create-reservation.service.js
@@ -1,12 +1,45 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('hotelier')
-    .factory('CreateReservationService', CreateReservationService);
+    angular.module('hotelier')
+        .factory('CreateReservationService', CreateReservationService);
 
-  function CreateReservationService() {
-    return {
+    CreateReservationService.$inject = ['$http'];
 
-    };
-  }
+    function CreateReservationService($http) {
+
+        return {
+            createReservation: createReservation
+        };
+
+        /**
+         * $http request that creates a new reservation when given the proper parameters
+         * @param  {String} checkinDate    Date given when guest Checks in
+         * @param  {String} checkoutDate   Date given when guest will checkout
+         * @param  {String} numberOfGuests Number of guests signed up for the reservation
+         * @param  {String} guestId        The guest who is reserving a rooms' id.
+         * @param  {String} roomId         The Id of the room being reserved
+         * @return {Promise}               [description]
+         */
+        function createReservation(checkinDate, checkoutDate, numberOfGuests, guestId, roomId) {
+            return $http({
+                url: 'https://hotelier-api-iron.herokuapp.com/api/Reservations',
+                data: {
+                    checkinDate: checkinDate,
+                    checkoutDate: checkoutDate,
+                    numberOfGuests: numberOfGuests,
+                    guestId: guestId,
+                    roomId: roomId
+                },
+                method: 'post',
+                headers: {
+                    Authorization: guestId
+                }
+                })
+                .then(function returnResponse(response) {
+                    console.log(response);
+                });
+
+        }
+    }
 }());
