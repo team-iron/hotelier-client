1fbf0e567997bc4819616b50e6c5d89bfc298d36
StephyStuff <stephanie.birch13@gmail.com>
2016-12-12 08:43:16 -0500

    Began writing sanity test.

--- test/specs/guest-list.service.spec.js
+++ test/specs/guest-list.service.spec.js
@@ -1,4 +1,20 @@
 (function() {
   'use strict';
-  
+
+var expect = chai.expect;
+var LoginService;
+
+describe('login service', function(){
+
+  beforeEach(module('hotelier'));
+
+  beforeEach(inject(function(_LoginService_) {
+    LoginService = _LoginService_;
+  }));
+
+  it('should expect 1 plus 1 to equal 1', function(){
+    expect(1).to.equal(1);
+  });
+});
+
 }());
