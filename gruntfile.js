27f57543431dd85b164ec187a4951fac68e7b9a8
StephyStuff <stephanie.birch13@gmail.com>
2016-12-13 16:55:09 -0500

    Added karma to registerTask test.

--- gruntfile.js
+++ gruntfile.js
@@ -151,6 +151,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('test', ['jshint', 'karma']);
   grunt.registerTask('default', [ 'clean', 'test', 'sass', 'copy', 'concat' ]);
 };
