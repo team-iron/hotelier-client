36e93cce19dfe1c481c2518d560f9eaa601ee8ee
ngalantowicz <ngalantowicz@gmail.com>
2016-12-13 21:15:18 -0500

    reservation testing setup

--- test/specs/reservations.service.spec.js
+++ test/specs/reservations.service.spec.js
@@ -5,22 +5,34 @@
 
     describe('reservations service', function() {
         var $httpBackend;
-        var mockLoginService;
+        var mockLoginService = {};
         var ReservationsService;
 
         beforeEach(module('hotelier'));
 
-        beforeEach(inject(function(_$httpBackend_,_LoginService_,_ReservationsService_) {
+        beforeEach(module(function($provide) {
+            $provide.value('LoginService', mockLoginService);
+        }));
+
+        beforeEach(inject(function(_$httpBackend_,_ReservationsService_) {
             $httpBackend = _$httpBackend_;
-            mockLoginService = _LoginService_;
             ReservationsService = _ReservationsService_;
 
             $httpBackend.whenGET('https://hotelier-api-iron.herokuapp.com/api/Reservations')
             .respond('Got Reservations');
 
+
+            // $httpBackend.whenGet('views/reservations.template.html')
+            // .respond('Got reservations view');
+
+            $httpBackend.whenGet('views/home.template.html')
+            .respond('Got reservations view');
+
             mockLoginService.loginYesNo = function() {
                 return ('Got Token');
             };
+
+
         }));
 
         it('should get reservations from API', function() {
